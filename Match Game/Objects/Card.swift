2cb23ba9005d9a94b7bc5ef63f3b3e4a7610ca3d
Alex Lopez <alex_lopez.es@icloud.com>
2019-08-24 15:55:25 +0200

    Added: Card object and Card model

---
+++ Match Game/Objects/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Match Game
+//
+//  Created by Alex Lopez on 24/08/2019.
+//  Copyright © 2019 Alex Lopez. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    
+    var imageName = ""
+    var isFlipped = false
+    var isMatched = false
+}
