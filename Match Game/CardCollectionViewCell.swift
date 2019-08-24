06f565be20b2fa7bf55debeaeeb0c1cf29d65045
Alex Lopez <alex_lopez.es@icloud.com>
2019-08-24 16:22:31 +0200

    Implemented: Collection View

---
+++ Match Game/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  Match Game
+//
+//  Created by Alex Lopez on 24/08/2019.
+//  Copyright © 2019 Alex Lopez. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
--- Match Game/ViewController.swift
+++ Match Game/ViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var model = CardModel()
+    var cardArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        cardArray = model.getCards()
     }
 
-
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
